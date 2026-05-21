3d38ad2301362d5eb4efd50423d3fe15a1382271
Sifat <sifat2109@gmail.com>
2026-05-21 13:44:54 +0000

    api key is "AIzaSyBRijUPvEnFXykWHvGLw0hNGSQN5VRcdnI"

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: "placeholder-api-key",
-  authDomain: "placeholder-auth-domain",
-  projectId: "placeholder-project-id",
-  storageBucket: "placeholder-storage-bucket",
-  messagingSenderId: "placeholder-messaging-sender-id",
-  appId: "placeholder-app-id"
+  apiKey: "AIzaSyBRijUPvEnFXykWHvGLw0hNGSQN5VRcdnI",
+  authDomain: "studio-7640622228-7f932.firebaseapp.com",
+  projectId: "studio-7640622228-7f932",
+  storageBucket: "studio-7640622228-7f932.firebasestorage.app",
+  messagingSenderId: "41896240116",
+  appId: "1:41896240116:web:a80008bee68ef206b68fa1"
 };
